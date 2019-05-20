dab36d224fb33a551e4e7575becadb161dec0154
Andżelika Domańska <domanskaa@student.mini.pw.edu.pl>
2019-05-21 00:48:19 +0200

    Add ActionLanguageTranslator implementation

--- RWSystem/RWSystem/Translation/ActionLanguageTranslator.cs
+++ RWSystem/RWSystem/Translation/ActionLanguageTranslator.cs
@@ -6,11 +6,73 @@ using System.Threading.Tasks;
 
 namespace RWSystem.Translation
 {
-    class ActionLanguageTranslator : BaseTranslator
+    class ActionLanguageTranslator
     {
-        public override string Translate(string[] tokens)
+        Dictionary<StatementType, BaseTranslator> translators = new Dictionary<StatementType, BaseTranslator>()
         {
-            return base.TranslateFormula(tokens);
+            { StatementType.Causes,
+                new CausesStatementTranslator() },
+            { StatementType.TypicallyCauses,
+                new TypicallyCausesStatementTranslator() },
+            { StatementType.Invokes,
+                new InvokesStatementTranslator() },
+            { StatementType.Triggers,
+                new TriggersStatementTranslator() },
+            { StatementType.Releases,
+                new ReleasesStatementTranslator() },
+            { StatementType.DisableBetween,
+                new DisbaleBetweenStatementTranslator() },
+            { StatementType.DisableWhen,
+                new DisableWhenStatementTranslator() }
+        };
+
+        Dictionary<StatementType, Token[]> characteristicTokens = new Dictionary<StatementType, Token[]>()
+        {
+            { StatementType.Causes,
+                new Token[] { Token.Causes} },
+            { StatementType.TypicallyCauses,
+                new Token[] { Token.Typically, Token.Causes } },
+            { StatementType.Invokes,
+                new Token[] { Token.Invokes } },
+            { StatementType.Triggers,
+                new Token[] { Token.Triggers } },
+            { StatementType.Releases,
+                new Token[] { Token.Releases } },
+            { StatementType.DisableBetween,
+                new Token[] { Token.Disabled, Token.Between } },
+            { StatementType.DisableWhen,
+                new Token[] { Token.Disabled, Token.When } }
+        };
+
+        public string Translate(string story)
+        {
+            string[] statements = story.Split(
+                new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            );
+
+            return String.Join("\n", statements.Select(s => TranslateStatement(s)));
+        }
+
+        string TranslateStatement(string statement)
+        {
+            string[] tokens = statement.Split(' ');
+            StatementType? statementType = GetStatementType(tokens);
+
+            if (!statementType.HasValue)
+                throw new Exception("Invalid statement");
+
+            return translators[statementType.Value].Translate(tokens);
+        }
+
+        StatementType? GetStatementType(string[] tokens)
+        {
+            foreach(StatementType statemenType in Enum.GetValues(typeof(StatementType)))
+            {
+                if (characteristicTokens[statemenType].Aggregate(true, (acc, token) => acc && tokens.Contains(token.Value)))
+                    return statemenType;
+            }
+            return null;
         }
     }
 }
---
+++ RWSystem/RWSystem/Translation/Token.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RWSystem.Translation
+{
+    public class Token
+    {
+        private Token(string value) { Value = value }
+
+        public string Value { get; set; }
+
+        public static Token Causes { get { return new Token("causes"); } }
+        public static Token Typically { get { return new Token("typically"); } }
+        public static Token Invokes { get { return new Token("invokes"); } }
+        public static Token Triggers { get { return new Token("triggers"); } }
+        public static Token Releases { get { return new Token("releases"); } }
+        public static Token Disabled { get { return new Token("disabled"); } }
+        public static Token Between { get { return new Token("between"); } }
+        public static Token When { get { return new Token("when"); } }
+    }
+}
