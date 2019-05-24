221442ce98c98b185c444230c75538493db79669
Maciek Gorczyca <maciek.gorczyca@poczta.fm>
2019-05-24 23:11:45 +0200

    next version of file

--- RWSystem/RWSystem/database.pl
+++ RWSystem/RWSystem/database.pl
@@ -1,9 +1,15 @@
-%% plik testowy z reprezetnacją przykładu
+%% historyjka
+typicallyCauses(work, [[workDone]], [[not_workDone], [rested]]).
 
-typicallyCauses(work, [[workDone]], [[\+ workDone], [rested]]).
+causes(work, [[not_rested]], [[not_workDone], [rested]]).
 
-causes(work, [[\+ rested]], [[\+ workDone], [rested]]).
-
-triggers(rest,[[\+ rested]]).
+triggers(rest,[[not_rested]]).
 
 causes(rest, [[rested]]).
+
+%%scenariusz
+
+observation( [ [ not_rested ], [not_workDone] ], 0 ).
+
+action( rest, 1 ).
+action(work, 2).
