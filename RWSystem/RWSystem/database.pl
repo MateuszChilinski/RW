d0435f19b0d17319dc4297b92627a003adae0f52
Maciek Gorczyca <maciek.gorczyca@poczta.fm>
2019-05-24 21:44:54 +0200

    test file for queries

---
+++ RWSystem/RWSystem/database.pl
@@ -0,0 +1,9 @@
+%% plik testowy z reprezetnacją przykładu
+
+typicallyCauses(work, [workDone], [\+ workDone, rested]).
+
+causes(work, [\+ rested], [\+ workDone, rested]).
+
+triggers(rest, [\+ rested]).
+
+causes(rest, [rested]).
