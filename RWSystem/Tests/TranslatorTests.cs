7288924f3d3b520d42f27ffba1cd7ba3fb950f50
Kamil Gacan <kamil.gacan@gmail.com>
2019-05-26 19:46:57 +0200

    Bootstrap tests project

---
+++ RWSystem/Tests/TranslatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RWSystem.Translation;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TranslatorTests
+    {
+        [Test]
+        public void Test()
+        {
+            var causesStatementTranslator = new CausesStatementTranslator();
+
+            var tokens = new string[] { "A", "causes", "dead", "if", "loaded" };
+
+            var translated = causesStatementTranslator.Translate(tokens);
+
+            Assert.AreEqual("causes(A, [[dead]], [[loaded]]).", translated);
+        }
+    }
+}
